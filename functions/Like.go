709939e8e53222e54b984fb75f9cf281c4fb1c49
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-07-02 12:55:32 -0500

    fix: correctly comment

--- functions/Like.go
+++ functions/Like.go
@@ -12,7 +12,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// LikePublication agrega el userID al array Likes de la publicación
+// LikePublication add a like to a publication ..
 func LikePublication(db *mongo.Database, publicationID string, userID int) (string, int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
